455b5ae5ea003e8162f24ade64f7d9cf39d13cc9
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-07-21 14:19:36 +0700

    feat: iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import JsxPreviewDemo from './JsxPreviewDemo';
+import React, { useState } from 'react';
+import JsxPreview from './JsxPreview';
+import { useEffect } from 'react';
 
 const App = () => {
+  const expectedOrigin = 'https://jsx-preview.vercel.app/';
+
+  const [selectedCode, setSelectedCode] = useState('');
+
+  useEffect(() => {
+    window.addEventListener('message', (event) => {
+      if (event.origin !== expectedOrigin) return;
+      console.log(event.data);
+      setSelectedCode(event.data);
+    });
+  }, []);
+
   return (
-    <div className="min-h-screen bg-gray-50 p-4">
-      <JsxPreviewDemo />
+    <div className="min-h-screen bg-gray-50">
+      <JsxPreview jsxCode={selectedCode} />
     </div>
   );
 };
 
 export default App;
+
